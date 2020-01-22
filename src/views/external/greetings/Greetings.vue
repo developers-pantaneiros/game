92fefabde26a196ef5f0345a10a76b792efbff21
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2020-01-22 00:20:02 -0300

    Alterando cor do botão

--- src/views/external/greetings/Greetings.vue
+++ src/views/external/greetings/Greetings.vue
@@ -2,7 +2,7 @@
   <section class="nes-container with-title is-centered">
     <p class="title">Game - {{version}}</p>
     <p>Seja bem-vindo ao Game. Nós temos muitas aventuras pela frente. Pressione o botão "Jogar" para começar.</p>
-    <router-link to="signin" class="nes-btn is-success">Jogar</router-link>
+    <router-link to="signin" class="nes-btn">Jogar</router-link>
   </section>
 </template>
 
