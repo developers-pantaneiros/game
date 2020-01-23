75825e324bca8ce002916c618b133c14217788e3
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2020-01-23 08:57:56 -0300

    Adicionando espaçamentos no body da aplicação

--- src/App.vue
+++ src/App.vue
@@ -8,12 +8,3 @@ export default {
   name: "App"
 };
 </script>
-
-<style lang="scss">
-body {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-</style>
