bc51fcbc46d20505c990c20621aef7e9ee9c8895
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2020-02-07 16:36:08 -0300

    Criando primeira versão da página classe

--- src/views/internal/student/class/classroom/ClassRoom.vue
+++ src/views/internal/student/class/classroom/ClassRoom.vue
@@ -3,9 +3,17 @@
         <div v-if="isLoading">
             <loading/>
         </div>
-        <div v-else class="center-text">
-            <h2 class="title">{{classFound.name}}</h2>
-            <p class="subtitle">{{classFound.description}}</p>
+        <div v-else>
+            <div class="margin-bottom-2 center-text">
+                <a href="#" class="nes-badge center-box margin-bottom-1">
+                    <span class="is-dark">{{classFound.name}}</span>
+                </a>
+                <p class="subtitle">{{classFound.description}}</p>
+                <p class="subtitle">Estudantes: {{students.length}}</p>
+            </div>
+            <div>
+                <class-room-teacher-card v-model="teacher" />
+            </div>
         </div>
     </class-wrapper>
 </template>
@@ -13,16 +21,18 @@
 <script>
 import Loading from "@/commons/components/Loading";
 import actionTypes from "@/commons/constants/action-types";
-import QRCode from "qrcode";
 import ClassWrapper from "../commons/ClassWrapper";
+import ClassRoomTeacherCard from "./components/ClassRoomTeacherCard";
 
 export default {
     name: "class-room",
-    components: { Loading, ClassWrapper },
+    components: { Loading, ClassWrapper, ClassRoomTeacherCard },
     data() {
         return {
             uid: "",
             classFound: {},
+            teacher: {},
+            students: [],
             isLoading: true
         };
     },
@@ -38,6 +48,8 @@ export default {
         async findClass() {
             try {
                 this.classFound = await this.$store.dispatch(actionTypes.FIND_CLASS, this.uid);
+                this.teacher = await this.$store.dispatch(actionTypes.FIND_USER_BY_REFERENCE, this.classFound.teacher);
+                this.students = await this.$store.dispatch(actionTypes.FIND_MANY_USERS_BY_REFERENCE, this.classFound.students);
                 await this.afterLoading();
             } catch (error) {
                 console.log(error);
---
+++ src/views/internal/student/class/classroom/components/ClassRoomTeacherCard.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="nes-container is-rounded teacher-card">
+        <div class="nes-avatar is-rounded is-large class-card__avatar">
+            <i class="nes-icon reddit is-large"></i>
+        </div>
+        <div>
+            <h4 class="class-card__title">{{value.name}}</h4>
+            <p class="class-card__subtitle">Professor</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "class-room-teacher-card",
+    props: {
+        value: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
