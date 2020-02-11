582d8bd4c15b653cbd0dcb79f6f2c60d8656ea40
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2020-02-11 16:51:27 -0300

    Criando lista de exercícios que os alunos devem resolver

--- src/views/internal/student/class/exercicies/ClassExercicies.vue
+++ src/views/internal/student/class/exercicies/ClassExercicies.vue
@@ -1,14 +1,51 @@
 <template>
     <class-wrapper>
-        <h1>Exercícios</h1>
+        <div class="margin-bottom-2 center-text">
+            <h2 class="title">Exercícios</h2>
+            <p class="subtitle">Lista de exercícios para resolver.</p>
+        </div>
+        <div class="flex">
+            <class-exercicies-card
+                v-for="item in exercicies"
+                v-bind:exercicie="item"
+                v-bind:key="item.index"/>
+        </div>
     </class-wrapper>
 </template>
 
 <script>
 import ClassWrapper from "../commons/ClassWrapper";
+import ClassExerciciesCard from "./components/ClassExerciciesCard";
 
 export default {
     name: "class-exercicies",
-    components: { ClassWrapper }
+    components: { ClassWrapper, ClassExerciciesCard },
+    data() {
+        return {
+            exercicies: [
+                {
+                    index: 1,
+                    name: 'Mudança de estados físicos da matéria'
+                },
+                {
+                    index: 2,
+                    name: 'Mudança de estados físicos da matéria'
+                },
+                {
+                    index: 3,
+                    name: 'Mudança de estados físicos da matéria'
+                }
+            ]
+        }
+    }
 }
 </script>
+
+<style lang="scss" scoped>
+    .flex {
+        display: flex;
+        align-items: center;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
+</style>
