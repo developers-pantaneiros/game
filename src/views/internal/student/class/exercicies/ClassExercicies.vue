a7aeb04814833c9425bbb2166f70fc7bea7a74ac
caarloshenrique <carlos.silva@novaandradina.org>
2020-02-19 21:15:59 -0300

    change nick exercises

--- src/views/internal/student/class/exercicies/ClassExercicies.vue
+++ src/views/internal/student/class/exercicies/ClassExercicies.vue
@@ -1,16 +1,17 @@
 <template>
-    <class-wrapper>
-        <div class="margin-bottom-2 center-text">
-            <h2 class="title">Exercícios</h2>
-            <p class="subtitle">Lista de exercícios para resolver.</p>
-        </div>
-        <div class="flex">
-            <class-exercicies-card
-                v-for="item in exercicies"
-                v-bind:exercicie="item"
-                v-bind:key="item.index"/>
-        </div>
-    </class-wrapper>
+  <class-wrapper>
+    <div class="margin-bottom-2 center-text">
+      <h2 class="title">Exercícios</h2>
+      <p class="subtitle">Lista de exercícios para resolver.</p>
+    </div>
+    <div class="flex">
+      <class-exercicies-card
+        v-for="item in exercicies"
+        v-bind:exercicie="item"
+        v-bind:key="item.index"
+      />
+    </div>
+  </class-wrapper>
 </template>
 
 <script>
@@ -18,34 +19,34 @@ import ClassWrapper from "../commons/ClassWrapper";
 import ClassExerciciesCard from "./components/ClassExerciciesCard";
 
 export default {
-    name: "class-exercicies",
-    components: { ClassWrapper, ClassExerciciesCard },
-    data() {
-        return {
-            exercicies: [
-                {
-                    index: 1,
-                    name: 'Mudança de estados físicos da matéria'
-                },
-                {
-                    index: 2,
-                    name: 'Mudança de estados físicos da matéria'
-                },
-                {
-                    index: 3,
-                    name: 'Mudança de estados físicos da matéria'
-                }
-            ]
+  name: "class-exercicies",
+  components: { ClassWrapper, ClassExerciciesCard },
+  data() {
+    return {
+      exercicies: [
+        {
+          index: 1,
+          name: "Mudança de estados físicos da matéria"
+        },
+        {
+          index: 2,
+          name: "Reações químicas"
+        },
+        {
+          index: 3,
+          name: "Teoria dos conjuntos"
         }
-    }
-}
+      ]
+    };
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-    .flex {
-        display: flex;
-        align-items: center;
-        flex-wrap: wrap;
-        justify-content: center;
-    }
+.flex {
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  justify-content: center;
+}
 </style>
