9c4298feb6f9a6ae2f75b9fffea20b1c3bf857c9
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2020-03-02 18:32:58 -0400

    Adicionando a biblioteca de drag and drop

--- src/views/internal/student/exercises/first/ExerciseFirst.vue
+++ src/views/internal/student/exercises/first/ExerciseFirst.vue
@@ -1,14 +1,47 @@
 <template>
     <div class="container container__full">
         <h1>Primeiro exercício!</h1>
+        <draggable v-model="myArray">
+            <transition-group>
+                <div v-for="element in myArray" :key="element.id">
+                    {{element.name}}
+                </div>
+            </transition-group>
+        </draggable>
     </div>
 </template>
 
 <script>
-    import ClassWrapper from "../../class/commons/ClassWrapper";
+    import draggable from "vuedraggable";
 
     export default {
         name: "exercise-first",
-        components: { ClassWrapper } 
+        components: { draggable },
+        data() {
+            return {
+                myArray: [
+                    {
+                        id: 1,
+                        name: 'Carlos Eduardo'
+                    },
+                    {
+                        id: 2,
+                        name: 'Carlos Henrique'
+                    },
+                    {
+                        id: 3,
+                        name: 'Nathália'
+                    },
+                    {
+                        id: 4,
+                        name: 'Yasmin'
+                    },
+                    {
+                        id: 5,
+                        name: 'Fábio'
+                    }
+                ]
+            }
+        }
     }
 </script>
