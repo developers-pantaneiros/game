fa214acbb609f09d089a4bea5ad32340a9dd74ac
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2020-03-14 17:30:38 -0400

    Adicionando mensagem de erro caso a lista nao esteja na ordem correta

--- src/views/internal/student/exercises/first/ExerciseFirst.vue
+++ src/views/internal/student/exercises/first/ExerciseFirst.vue
@@ -6,13 +6,17 @@
             <audio-button :tagId="'list-exercises'" />
         </div>
         <div>
-            <draggable v-model="physicalStates">
-                <transition-group class="draggable-list">
+            <transition name="fade">
+                <p class="paragraph paragraph--error margin-top-1 margin-bottom-1" v-show="error">{{error}}</p>
+            </transition>
+            <draggable v-model="physicalStates" :move="onMoveElement">
+                <transition-group class="draggable-list margin-top-2">
                     <div class="draggable-list__item" v-for="element in physicalStates" :key="element.id">
                         <img class="draggable-list__item--photo" :src="element.photo" :alt="element.value" v-if="element.photo">
                         <p class="draggable-list__item--text">{{ element.value }}</p>
                     </div>
                 </transition-group>
+                <button slot="footer" class="nes-btn is-success margin-top-2 right" @click="checkPhysicalStatesOrder">Finalizar</button>
             </draggable>
         </div>
     </div>
@@ -31,6 +35,10 @@
         components: { draggable },
         data() {
             return {
+                timer: {
+                    seconds: 0
+                },
+                error: '',
                 physicalStates: [
                     {
                         id: 0,
@@ -67,11 +75,47 @@
                         photo: rain,
                         value: 'Líquido'
                     }
-                ]
+                ],
+                correctOrderIds: [0, 1, 2, 3, 4, 5, 6]
             }
         },
         created() {
-            this.physicalStates = shuffle(this.physicalStates)
+            this.shufflePhysicalStates();
+            this.initTimer();
+        },
+        methods: {
+            shufflePhysicalStates() {
+                this.physicalStates = shuffle(this.physicalStates);
+            },
+            initTimer() {
+                this.interval = setInterval(() => { this.updateTimer() }, 1000);
+            },
+            updateTimer() {
+                this.timer.seconds += 1;
+            },
+            onMoveElement() {
+                this.error = ''
+            },
+            checkPhysicalStatesOrder() {
+                if (this.isPysicalStatesInCorrectOrder()) {
+                    console.log('Ordem correta!');
+                    console.log(`Tempo: ${this.timer.seconds} segundos.`)
+                    this.destroyInterval()
+                } else {
+                    this.error = 'Ops, ainda restam elementos embaralhados. Por favor, tente novamente.'
+                }
+            },
+            isPysicalStatesInCorrectOrder() {
+                for (let i = 0; i < this.physicalStates.length; i++) {
+                    if (this.physicalStates[i].id !== this.correctOrderIds[i]) {
+                        return false
+                    }
+                }
+                return true
+            },
+            destroyInterval() {
+                clearInterval(this.interval)
+            }
         }
     }
 </script>
