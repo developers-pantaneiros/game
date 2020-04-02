391b02934d62b4cd7b1b4d92d818345f44a739f3
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2020-04-01 21:36:43 -0400

    Adicionei ícone no botão de voltar

--- src/views/external/reset/components/ResetPasswordFormActions.vue
+++ src/views/external/reset/components/ResetPasswordFormActions.vue
@@ -5,16 +5,26 @@
         <input v-model="email" type="email" id="email_input" class="nes-input form-input" required />
         <p style="margin-top: 20px"></p>
         <button type="submit" class="nes-btn is-success full-width margin-bottom-1" @click="resetPassword">Redefinir</button>
-        <router-link to="/signin" class="nes-btn full-width">Voltar</router-link>
+        <router-link to="/signin" class="nes-btn button-with-icon full-width">
+            <font-awesome-icon size="lg" :icon="icons.faAngleLeft"/>
+            Voltar
+        </router-link>
     </div>
 </template>
 
 <script>
+    import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+    import { faAngleLeft } from '@fortawesome/free-solid-svg-icons'
+
     export default {
         name: 'reset-password-form-actions',
+        components: { FontAwesomeIcon },
         data() {
             return {
-                email: ''
+                email: '',
+                icons: {
+                    faAngleLeft
+                }
             }
         },
         methods: {
